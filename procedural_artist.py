cd37a41bbbfd8f230e99d856c9663154651c7810
Scotty Jacobson <ScottyMJacobson@gmail.com>
2014-09-17 22:14:24 -0400

    began to create Artist class

--- procedural_artist.py
+++ procedural_artist.py
@@ -30,27 +30,45 @@ def paint(index):
 class RestrictedPixelMap:
     def __init__(self, pix_map, (width, height)):
         self.pix_map = pix_map
+        print type(pix_map)
         self.width = width
         self.height = height
         self.lock_map = [[threading.Lock()]*width for i in range(height)]
+
     def draw_to_pixel (self, new_pixel, row, column):
         success = False
-        self.lock_map[row][column].acquire()
+        self.lock_map[column][row].acquire()
         #acquire the lock to that pixel
-        this_pixel = self.pix_map[row,column]
+        this_pixel = self.pix_map[column,row]
         if this_pixel == (255,255,255):
-            this_pixel = new_pixel
+            self.pix_map[column,row] = new_pixel
+            print "Changed"
             success = True
+        else:
+            print "Failed"
         self.lock_map[row][column].release()
         return success
 
+class Artist:
+    def __init__(self, index, color, max_steps):
+        self.index=index
+        self.color=color
+        self.max_steps=max_steps
+        self.steps_taken = 0
+        self.pixels_i_own = 0
+
+    def ready_set_paint (self, restricted_canvas):
+        while self.steps_taken < self.max_steps:
+            self.steps_taken += 1
+
+
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument("number_of_threads", type=int)
+    parser.add_argument("number_of_steps", type=int)
 
     canvas = Image.new('RGB', (512,512), color=(255,255,255))
-
     canvas_map = canvas.load()
-
     restricted_canvas = RestrictedPixelMap(canvas_map, canvas.size)
 
     restricted_canvas.draw_to_pixel((255,1,1), 10, 10)
@@ -61,3 +79,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
